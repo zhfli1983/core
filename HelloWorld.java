9c239fa275fbdad9a6b73d109cb8fb4269e07b13
bony <zhfli1983@163.com>
2016-09-28 20:57:01 +0800

    add new file

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+     public static void main(String[] args){
+		 System.out.println("this is the test");
+	 }
+//hello
+	 public void print(){
+	     Sysout.out.println("this is a test");
+	 }
+
+ //define a new method
+	 public void info(){
+	     Sysout.out.println("this is a test");
+	 }
+      
+  //add the comment in the server
+     public void getInfo(){
+	     //
+	 }
+}
